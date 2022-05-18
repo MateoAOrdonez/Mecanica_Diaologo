1562a6e34d867681b1b2ed8a9ecb76893c2da141
camilo520 <camilo.cuevas@uao.edu.co>
2022-05-18 15:19:43 -0500

    feat: textures

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,6 +6,7 @@ public class CharacterController : MonoBehaviour
 {
     private float movementSpeed = 8f;
     private float movement;
+    private Animator animator;
 
     private float JumpForce = 12f;
 
@@ -14,19 +15,36 @@ public class CharacterController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       movement = Input.GetAxis("Horizontal");
-       transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
-
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidBody.velocity.y) < 0.001f)
+        if (Dialogue.didDialogueStart == false)
         {
-            rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+
+            movement = Input.GetAxis("Horizontal");
+            transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+
+            
+
+            if (!Mathf.Approximately(0, movement))
+            {
+                transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+            }
+
+            if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidBody.velocity.y) < 0.001f)
+            {
+                rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+                animator.SetBool("IsJumping", true);
+            }
+
+            
         }
+
+        animator.SetFloat("Speed", Mathf.Abs(movement));
 
     }
 }
